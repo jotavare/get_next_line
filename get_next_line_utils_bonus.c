6095bdf69febc88d91481775d9594c2c2298189c
João Oliveira <38820667+jotavare@users.noreply.github.com>
2022-12-30 20:52:33 +0000

    feat: add get_next_line, its utils and the bonus variants

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jotavare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 00:43:10 by jotavare          #+#    #+#             */
+/*   Updated: 2022/12/18 00:43:11 by jotavare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+//	fills 'n' bytes of a memory area 's'
+//	with null characters.
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*((unsigned char *)s + i) = '\0';
+		i++;
+	}
+}
+
+//	Calculates the lenght of a string 'str'
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+//	Function that searches for a character 'c'
+//	in a string 's' and returns a pointer to
+//	that character
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (s == NULL)
+		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == '\0' && c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+//	Function that takes 2 strings, 's1' and 's2',
+//	and joins them together in a new string 'str'
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = (char *)malloc(len * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1 && *s1)
+	{
+		str[i++] = *s1;
+		s1++;
+	}
+	while (s2 && *s2)
+	{
+		str[i++] = *s2;
+		s2++;
+	}
+	str[i] = '\0';
+	return (str);
+}
