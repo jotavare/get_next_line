72e7b5db3f061b6b0d237554d608e8fe3e134ec2
João Oliveira <38820667+jotavare@users.noreply.github.com>
2023-03-10 23:31:05 +0000

    docs: add the README and reorganise the sources into get_next_line/

--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include    "get_next_line_bonus.h"
 
 //	fills 'n' bytes of a memory area 's'
 //	with null characters.
